0a5ea80593baadcb8fbc0319019393e0ec3d4afa
Danilo Bonometti <danilobonometti@udec.cl>
2020-08-11 14:39:55 -0400

    agregados archivos base del proyecto

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "pr_quad_tree.h"
+
+int main(int argc, char const *argv[])
+{
+	dsa::pr_quad_tree q_tree;
+
+	return 0;
+}
---
+++ pr_quad_tree.h
@@ -0,0 +1,16 @@
+#ifndef PR_QUAD_TREE_H
+#define PR_QUAD_TREE_H
+
+namespace dsa {
+
+class pr_quad_tree
+{
+public:
+	pr_quad_tree() {}
+	~pr_quad_tree() {}
+	
+};
+	
+} // namespace dsa
+
+#endif
